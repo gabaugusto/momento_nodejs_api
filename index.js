99a01adb7fdcc6d87386c8ef007dfce657130b83
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2023-10-23 20:33:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,47 +2,53 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
-const port = 3000; // Defina a porta que desejar
+
+//é necessário configurar o middleware express.json() para analisar o corpo da solicitação JSON. 
+app.use(express.json()); //
+
+
+const port = 3000; // Defina a porta que deseja executar o seu projeto. 
 
 // Configuração do banco de dados MySQL
 const db = mysql.createConnection({
   host: 'localhost',  // Endereço do servidor MySQL
   user: 'root', // Seu nome de usuário MySQL
-  password: '', // Sua senha do MySQL
+  password: 'password', // Sua senha do MySQL
   database: 'oscar_database' // Nome do banco de dados
 });
 
+// Tenta conectar o MySQL. 
 db.connect((err) => {
   if (err) {
     console.error('Erro ao conectar ao MySQL: ' + err);
     return;
   }
-  console.log('Conexão ao MySQL estabelecida');
+  console.log('Conexão ao MySQL estabelecida'); // Sucesso
 });
 
-// Defina os endpoints da API
+// Definir os endpoints da API
 
-// Rota para obter todos os filmes
-app.get('/filmes', (req, res) => {
-  const query = 'SELECT * FROM filmes';
+// Rota para obter todos os registros
+app.get('/oscar', (req, res) => {
+  const query = 'SELECT * FROM oscar';
   db.query(query, (err, results) => {
     if (err) {
-      console.error('Erro ao buscar filmes: ' + err);
-      res.status(500).json({ error: 'Erro ao buscar filmes' });
+      console.error('Erro ao buscar registros: ' + err);
+      res.status(500).json({ error: 'Erro ao buscar registros' });
       return;
     }
     res.json(results);
   });
 });
 
-// Rota para obter um filme por ID
-app.get('/filmes/id/:id', (req, res) => {
+// Rota para obter um registro por ID
+app.get('/oscar/id/:id', (req, res) => {
   const { id } = req.params;
-  const query = 'SELECT * FROM filmes WHERE id_registro = ?';
+  const query = 'SELECT * FROM oscar WHERE id_registro = ?';
   db.query(query, [id], (err, results) => {
     if (err) {
-      console.error('Erro ao buscar filme: ' + err);
-      res.status(500).json({ error: 'Erro ao buscar filme' });
+      console.error('Erro ao buscar registro: ' + err);
+      res.status(500).json({ error: 'Erro ao buscar registro' });
       return;
     }
     if (results.length === 0) {
@@ -53,25 +59,43 @@ app.get('/filmes/id/:id', (req, res) => {
   });
 });
 
-// Rota para obter um filme por ID
-app.get('/filmes/nome/:name', (req, res) => {
+// Rota para obter um registro por ano da filmagem
+app.get('/oscar/ano_filmagem/:ano', (req, res) => {
+  const { ano } = req.params;
+  const query = 'SELECT * FROM oscar WHERE ano_filmagem = ?';
+  db.query(query, [id], (err, results) => {
+    if (err) {
+      console.error('Erro ao buscar registro: ' + err);
+      res.status(500).json({ error: 'Erro ao buscar registro' });
+      return;
+    }
+    if (results.length === 0) {
+      res.status(404).json({ error: 'Filme não encontrado' });
+    } else {
+      res.json(results[0]);
+    }
+  });
+});
+
+// Rota para obter um resgistro por nome do indicado
+app.get('/oscar/nome/:name', (req, res) => {
     const { name } = req.params;
-    const query = 'SELECT * FROM filmes WHERE id_registro = ?';
+    const query = `SELECT * FROM oscar WHERE nome_do_indicado LIKE "%${name}%" `;
     db.query(query, [name], (err, results) => {
       if (err) {
-        console.error('Erro ao buscar filme: ' + err);
-        res.status(500).json({ error: 'Erro ao buscar filme' });
+        console.error('Erro ao buscar registro: ' + err);
+        res.status(500).json({ error: 'Erro ao buscar registro' });
         return;
       }
       if (results.length === 0) {
-        res.status(404).json({ error: 'Filme não encontrado' });
+        res.status(404).json({ error: 'Registro não encontrado' });
       } else {
-        res.json(results[0]);
+        res.json(results);
       }
     });
   });
 
-// Inicie o servidor
+// Finalmente, iniciamos o servidor e aplicação estará disponível. 
 app.listen(port, () => {
-  console.log(`Servidor Express rodando na porta ${port}`);
+  console.log(`Servidor Express rodando na porta ${port}. Acesso através de http://localhost:${port}/`);
 });
